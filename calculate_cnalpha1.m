3083cf4fc7139d75ef409db98d5236433c4b1e0a
Usmaan Y <usmaan1901@gmail.com>
2023-01-30 14:52:50 +0000

    flight sim created
    - created a flight simulator simulink file
    - added a subsystem for atmospheric stuff but need to check how to added wind rates
    - created a cnalpha function to use in conjunction with a barrowmans script

---
+++ calculate_cnalpha1.m
@@ -0,0 +1,12 @@
+function Cnalpha1 = calculate_cnalpha1(M)
+% Ouput - Cnalpha
+% Input - Mach Number 
+
+load('barrowman.mat') % need to create a file to compute barrowmans equations
+
+% prandtl glauert correction (should work for both sub and supersonic 
+beta = sqrt(abs(1-M^2));
+
+Cnalpha1 = (2 * pi * AR * (Af/Ar)) / (2 + sqrt(4 + ((beta * AR /(cos(gamac)))^2 )));
+
+end
--- nimbus.m
+++ nimbus.m
@@ -1,29 +1,35 @@
 % nimbus initialisation script 
 % lets try and keep to si units !!
 
-% Body 
+% add section for wind conditions i guess
+
+% Body (main airframe)
 body.l = 
 body.d = 
 
-% NoseCone 
+% NoseCone (von Karman)
 nose.l =
 nose.d = 
 
-% Fins (Flat Plate)
-fin.number = 
-fin.Croot = 
-fin.Ctip = 
-fin.span = 
-fin.t_max = 
-fin.sweepLE = 
+% Tail 
+tail.l = 
+tail.d_root = 
+tail.d_tip = 
 
-% Canards (NACA 0012)
-canard.number = 
+% Fins (Flat Plate - trapezoidal)
+fin.number = 3;
+fin.Croot = 0.42;
+fin.Ctip = 0.20;
+fin.span = 0.23;
+fin.t_max = 0.04;
+fin.sweepLE = 27.8; % deg
+
+% Canards (NACA 0012 - swept maybe ?!)
+canard.number = 3;
 canard.Croot = 
 canard.Ctip = 
 canard.span = 
 canard.t_max = 
-canard.sweepLE = 
 
 % Engine Parameters
 engine.isp = 
